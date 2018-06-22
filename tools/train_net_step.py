2fd9fb373b4491bcc26287fa8b6e744e84013993
Prinsphield <xiaotaihong@126.com>
2018-06-22 10:06:36 +0800

    minor

--- tools/train_net_step.py
+++ tools/train_net_step.py
@@ -130,8 +130,8 @@ def save_ckpt(output_dir, args, step, train_size, model, optimizer):
         'step': step,
         'train_size': train_size,
         'batch_size': args.batch_size,
-        'model': {key: val.cpu() for key, val in model.state_dict().items()},
-        'optimizer': {key: val.cpu() for key, val in optimizer.state_dict().items()}}, save_name)
+        'model': model.state_dict(),
+        'optimizer': optimizer.state_dict()}, save_name)
     logger.info('save model: %s', save_name)
 
 
